faaa8bd7779d48dc22f0fb780eb7c404c92111a6
rikvanblyat <90496417+rikvanblyat@users.noreply.github.com>
2025-06-09 16:28:29 +0800

    Delete New folder directory

--- New folder/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Chatbot KIK", layout="wide")
-
-st.title("📌 Chatbot KIK – Versi Permulaan")
-st.write("Selamat datang ke Chatbot KIK. Ini adalah paparan asas untuk permulaan.")
